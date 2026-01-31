d3757fdb065e8980c5ea32339c46cbb773b34de6
Emmanuel Ayomikun <e.atigbi@alustudent.com>
2026-01-31 21:34:03 +0100

    Linear_search_slow_method

---
+++ dsa/linear_search.py
@@ -0,0 +1,7 @@
+# dsa/linear_search.py
+
+def linear_search(transactions, target_id):
+    for transaction in transactions:
+        if transaction["id"] == target_id:
+            return transaction
+    return None
