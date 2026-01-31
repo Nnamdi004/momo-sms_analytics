b562eae20fe0750efea6a884bc3e64fd154f7b0e
Emmanuel Ayomikun <e.atigbi@alustudent.com>
2026-01-31 21:36:47 +0100

    dict_search_fast_lookup

---
+++ dsa/dict_lookup.py
@@ -0,0 +1,7 @@
+# dsa/dict_lookup.py
+
+def build_transaction_dict(transactions):
+    return {tx["id"]: tx for tx in transactions}
+
+def dict_lookup(transactions_dict, target_id):
+    return transactions_dict.get(target_id)
