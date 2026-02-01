4612323fd7156056ac5463c03c9c834044a5cbd0
Nnamdi004 <c.onugha@alustudent.com>
2026-02-01 12:23:31 +0100

    Updated sample_data.py

--- dsa/sample_data.py
+++ dsa/sample_data.py
@@ -1,6 +1,10 @@
-# dsa/sample_data.py
-
 transactions = [
-    {"id": f"tx{i}", "type": "transfer", "amount": i * 1000, "sender": "Alice", "receiver": "Bob"}
+    {
+        "id": i,
+        "type": "transfer",
+        "amount": i * 1000,
+        "sender": "Alice",
+        "receiver": "Bob"
+    }
     for i in range(1, 21)
 ]
