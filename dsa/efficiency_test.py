2e172d7363a24466d68bdd77dd5904b5c0dfdda8
Nnamdi004 <c.onugha@alustudent.com>
2026-02-01 12:24:41 +0100

    Updated efficiency_test.py
    Updated timing methods to use time.perf_counter for better accuracy and changed target_id to an integer.

--- dsa/efficiency_test.py
+++ dsa/efficiency_test.py
@@ -1,22 +1,20 @@
-# dsa/efficiency_test.py
-
 import time
 from sample_data import transactions
 from linear_search import linear_search
 from dict_lookup import build_transaction_dict, dict_lookup
 
-target_id = "tx20"
+target_id = 20
 
 # Linear search timing
-start = time.time()
+start = time.perf_counter()
 linear_result = linear_search(transactions, target_id)
-linear_time = time.time() - start
+linear_time = time.perf_counter() - start
 
 # Dictionary lookup timing
 transactions_dict = build_transaction_dict(transactions)
-start = time.time()
+start = time.perf_counter()
 dict_result = dict_lookup(transactions_dict, target_id)
-dict_time = time.time() - start
+dict_time = time.perf_counter() - start
 
 print("Linear Search Result:", linear_result)
 print("Linear Search Time:", linear_time)
