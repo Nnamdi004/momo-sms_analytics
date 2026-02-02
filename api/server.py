bb61a49c647cbfa2f015e195d95c4ebea9402691
Nnamdi004 <c.onugha@alustudent.com>
2026-02-02 11:28:44 +0100

    Add sys and os imports to server.py

--- api/server.py
+++ api/server.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import base64
